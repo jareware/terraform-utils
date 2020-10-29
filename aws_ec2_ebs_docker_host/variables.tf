4bb0c8cb872041f867fd72aaa527010f961d93b4
Jarno Rantanen <jarno@jrw.fi>
2020-10-29 20:55:00 +0200

    Update default instance_ami in aws_ec2_ebs_docker_host module.

--- aws_ec2_ebs_docker_host/variables.tf
+++ aws_ec2_ebs_docker_host/variables.tf
@@ -32,7 +32,7 @@ variable "instance_type" {
 
 variable "instance_ami" {
   description = "See https://cloud-images.ubuntu.com/locator/ec2/ for options"
-  default     = "ami-0dad359ff462124ca" # i.e. 20.04 LTS on eu-west-1; see https://cloud-images.ubuntu.com/locator/ec2/
+  default     = "ami-0c4c42893066a139e" # Ubuntu 20.04 LTS (eu-west-1, amd64, hvm:ebs-ssd, 2020-09-24)
 }
 
 variable "ssh_private_key_path" {
